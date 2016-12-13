0653cdd38419ce1e9c44ff2494304fb7d202d71c
grimsdottir01 <xboxbum@gmail.com>
2016-12-12 23:23:48 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,19 +5,18 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;  botRegexsu = /^\sign up$/; 
+      botRegex = /^\/cool guy$/; botRegexsu= /^\sign up$/; 
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
-  }
-  
+  }  
    if(request.text && botRegexsu.test(request.text)) {
     this.res.writeHead(200);
     postMessage("this is a test");
     this.res.end();
-   }
+   
   } else {
     console.log("don't care");
     this.res.writeHead(200);
