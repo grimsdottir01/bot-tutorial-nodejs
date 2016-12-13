b62aea2d341f918f8df32a2edb11a8d2c7d233e7
grimsdottir01 <xboxbum@gmail.com>
2016-12-12 22:57:20 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,12 +7,12 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guy$/;
       botRegexSb = /^\signup$/;
-  else if(request.text && signup$.test(request.text)) {
-this.res.writeHead(200);
-postMessage("This is a test");
-this.res.end();
+  
 }
-
+  if(request.text && signup$.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("This is a test");
+    this.res.end();
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
